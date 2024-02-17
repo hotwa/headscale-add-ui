e140b79881f803e4d0b5df445664a78d26455cbc
hotwa <ze.ga@qq.com>
2024-02-17 22:11:20 +0800

    activate

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # 激活虚拟环境
-source /app/.venv/bin/activate
-
-# 在后台启动 headscale 服务，但不使用 & 放入后台，而是让其在前台运行
-headscale serve &
+. /app/.venv/bin/activate
 
 # 在后台启动 Headscale-WebUI
-gunicorn -w 4 -b 0.0.0.0:5000 server:app &
+gunicorn -w 1 -b 0.0.0.0:5000 server:app &
 
 # 等待任何进程退出
 wait -n
