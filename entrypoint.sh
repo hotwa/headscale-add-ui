1d2d318a65ed750ea3a35cb1d295b8dd90a5deb0
hotwa <ze.ga@qq.com>
2024-02-18 01:08:17 +0800

    u log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 启动需要 root 权限的服务
-headscale serve &
+
+# 启动需要 root 权限的服务，并将输出重定向到日志文件
+headscale serve > /var/lib/headscale/logfile.log 2>&1 &
 
 # 切换回 uid 为 1000 用户执行 CMD
 exec gosu 1000 "$@"
